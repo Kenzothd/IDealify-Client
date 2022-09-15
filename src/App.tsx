f9e5a5b425b182cc2ab39589ec1136a04f164ba6
Faith Ye <faith.ye@hotmail.com>
2022-09-15 16:36:26 +0800

    completed vendor project table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import VendorProjectTable from "./components/vendor/VendorProjectTable";
 
 function App() {
   return (
     <div>
       <h1>Hello Faith</h1>
+      <VendorProjectTable/>
     </div>
   );
 }
