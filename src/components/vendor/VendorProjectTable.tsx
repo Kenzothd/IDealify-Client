db529da7210a42d3b821fd796f16a12718621fc2
Faith Ye <faith.ye@hotmail.com>
2022-09-15 17:21:34 +0800

    adding resfresh button

--- src/components/vendor/VendorProjectTable.tsx
+++ src/components/vendor/VendorProjectTable.tsx
@@ -1,77 +1,96 @@
-import React, {useEffect, useState, FC} from 'react';
-import { DataGrid, GridColDef, GridValueGetterParams, GridApi} from '@mui/x-data-grid';
-import { Button } from '@mui/material';
+import React, { useEffect, useState, FC } from "react";
+import {
+  DataGrid,
+  GridColDef,
+  GridValueGetterParams,
+  GridApi,
+} from "@mui/x-data-grid";
+import { Button } from "@mui/material";
 import axios from "axios";
 import urlcat from "urlcat";
 const SERVER = import.meta.env.VITE_SERVER;
 import format from "date-fns/format";
 interface IActivities {
-    _id: string,
-    activityTitle: string,
-    activityDescription: string,
-    activityStartDate: Date,
-    activityEndDate: Date,
-    personInCharge: string,
-    status: string,
-    photos: Array<string>,
-    __v?: number,
+  _id: string;
+  activityTitle: string;
+  activityDescription: string;
+  activityStartDate: Date;
+  activityEndDate: Date;
+  personInCharge: string;
+  status: string;
+  photos: Array<string>;
+  __v?: number;
 }
 
-const VendorProjectTable : FC = ()=>{
-    // we can also leave it uninitialized but add in <IActivities[] | undefined>
-    const [activities, setActivities]= useState<IActivities[]>([]); 
+const VendorProjectTable: FC = () => {
+  // we can also leave it uninitialized but add in <IActivities[] | undefined>
+  const [activities, setActivities] = useState<IActivities[]>([]);
+  const [refreshActivities, setRefreshActivities] = useState<boolean>(false);
 
-    useEffect(()=>{
-        const url = urlcat(SERVER, "/activities")
-        axios.get(url).then(res=>setActivities(res.data)).catch((err)=>console.log(err))
-    },[])
-
-    const columns: GridColDef[] = [
-        { field: 'activityTitle', headerName: 'Activity Title', width: 200 },
-        { field: 'status', headerName: 'Status', width: 200 },
-        { field: 'activityStartDate', headerName: 'Start Date', width: 200 },
-        { field: 'activityEndDate', headerName: 'End Date', width: 200 },
-        { field: 'personInCharge', headerName: 'Person In Charge', width: 200 },
-        {
-            field: "action",
-            headerName: "Click To View",
-            sortable: false,
-            width: 200,
-            renderCell: (params) => {
-                const onClick = (e: React.MouseEvent) => {
-                    console.log(e.target)
-                    console.log(params.row)
-                  };
-                  return <Button onClick={onClick}>Click</Button>;
-            }
-          },
-      ];
-      
-      const rows = activities.map((activity)=>{
-        return {
-            id: activity._id,
-            activityTitle: activity.activityTitle,
-            status: activity.status,
-            activityStartDate: format(new Date(activity.activityStartDate),"dd/MM, hh:mm a"),
-            activityEndDate: format(new Date(activity.activityEndDate),"dd/MM, hh:mm a"),
-            personInCharge: activity.personInCharge,
-
-        }
+  useEffect(() => {
+    const url = urlcat(SERVER, "/activities");
+    axios
+      .get(url)
+      .then((res) => {
+        setActivities(res.data);
       })
+      .catch((err) => console.log(err));
+  }, [refreshActivities]);
 
-        return (
-          <div style={{ height: 400, width: '100%' }}>
-            <DataGrid
-              rows={rows}
-              columns={columns}
-              pageSize={10}
-              rowsPerPageOptions={[5]}
-              
-            />
-            {/* <pre>{JSON.stringify(activities, null, 2)}</pre> */}
-          </div>
-        );
-      }
+  const columns: GridColDef[] = [
+    { field: "activityTitle", headerName: "Activity Title", width: 200 },
+    { field: "status", headerName: "Status", width: 200 },
+    { field: "activityStartDate", headerName: "Start Date", width: 200 },
+    { field: "activityEndDate", headerName: "End Date", width: 200 },
+    { field: "personInCharge", headerName: "Person In Charge", width: 200 },
+    {
+      field: "action",
+      headerName: "Click To View",
+      sortable: false,
+      width: 200,
+      renderCell: (params) => {
+        const onClick = (e: React.MouseEvent) => {
+          console.log(e.target);
+          console.log(params.row);
+        };
+        return <Button onClick={onClick}>Click</Button>;
+      },
+    },
+  ];
 
+  const rows = activities.map((activity) => {
+    return {
+      id: activity._id,
+      activityTitle: activity.activityTitle,
+      status: activity.status,
+      activityStartDate: format(
+        new Date(activity.activityStartDate),
+        "dd/MM, hh:mm a"
+      ),
+      activityEndDate: format(
+        new Date(activity.activityEndDate),
+        "dd/MM, hh:mm a"
+      ),
+      personInCharge: activity.personInCharge,
+    };
+  });
+
+  const refreshActivitiesHandler = (): void => {
+    setRefreshActivities(!refreshActivities);
+  };
+
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[5]}
+      />
+      <Button onClick={refreshActivitiesHandler}>Refresh Activities</Button>
+      {/* <pre>{JSON.stringify(activities, null, 2)}</pre> */}
+    </div>
+  );
+};
 
 export default VendorProjectTable;
