d5704f07f15f670ff244a70825233936814bea99
Faith Ye <faith.ye@hotmail.com>
2022-09-16 11:33:39 +0800

    updating the activity data table

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./App.css";
 import VendorProjectTable from "./components/vendor/VendorProjectTable";
 import VendorAccount from "./components/vendor/VendorAccount";
 
+
 function App() {
   return (
     <>
       <VendorProjectTable />
-      <VendorAccount />
+      {/* <VendorAccount /> */}
+   
     </>
   );
 }
--- src/components/vendor/VendorProjectTable.tsx
+++ src/components/vendor/VendorProjectTable.tsx
@@ -5,7 +5,7 @@ import {
   GridValueGetterParams,
   GridApi,
 } from "@mui/x-data-grid";
-import { Button } from "@mui/material";
+import { Button, Menu, Typography } from "@mui/material";
 import axios from "axios";
 import urlcat from "urlcat";
 const SERVER = import.meta.env.VITE_SERVER;
@@ -18,7 +18,7 @@ interface IActivities {
   activityEndDate: Date;
   personInCharge: string;
   status: string;
-  photos: Array<string>;
+  photos: string[];
   __v?: number;
 }
 
@@ -38,22 +38,104 @@ const VendorProjectTable: FC = () => {
   }, [refreshActivities]);
 
   const columns: GridColDef[] = [
-    { field: "activityTitle", headerName: "Activity Title", width: 200 },
-    { field: "status", headerName: "Status", width: 200 },
-    { field: "activityStartDate", headerName: "Start Date", width: 200 },
-    { field: "activityEndDate", headerName: "End Date", width: 200 },
-    { field: "personInCharge", headerName: "Person In Charge", width: 200 },
+    {
+      field: "activityTitle",
+      headerName: "Activity Title",
+      width: 450,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 200,
+      headerAlign: "center",
+      align: "center",
+      renderCell: (params) => {
+        const onClick = (e: React.MouseEvent) => {
+          console.log(e.target);
+          console.log(params.row.status);
+        };
+
+        // (params.row.status === "Upcoming" && "purple") ||
+        //       (params.row.status === "Pending" && "gray") ||
+        //       (params.row.status === "In Progress" && "yellow") ||
+        //       (params.row.status === "Completed" && "green") ||
+        //       (params.row.status === "Cancelled" && "red"),
+
+        let bgColor: string;
+        switch (params.row.status){
+          case "Upcoming":
+           bgColor = "purple"; 
+           break;
+          case "Pending":
+            bgColor = "gray";
+            break;
+          case "In Progress":
+            bgColor ="orange";
+            break;
+          case "Completed":
+            bgColor="green";
+            break;
+          case "Cancelled":
+            bgColor= "red";
+            break;
+          default: 
+            bgColor= "gray";
+            break;
+        }
+        return (
+          <Typography
+            sx={{
+              fontWeight: "bold",
+              fontSize: "0.75rem",
+              color: "white",
+              borderRadius: 8,
+              padding: "3px 10px",
+              display: "inline-block",
+              backgroundColor: bgColor
+            }}
+          >
+            {params.row.status}
+          </Typography>
+        );
+      },
+    },
+    {
+      field: "activityStartDate",
+      headerName: "Start Date",
+      width: 200,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "activityEndDate",
+      headerName: "End Date",
+      width: 200,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "personInCharge",
+      headerName: "Person In Charge",
+      width: 200,
+      headerAlign: "center",
+      align: "center",
+    },
     {
       field: "action",
       headerName: "Click To View",
       sortable: false,
       width: 200,
+      headerAlign: "center",
+      align: "center",
+      filterable: false,
       renderCell: (params) => {
         const onClick = (e: React.MouseEvent) => {
           console.log(e.target);
           console.log(params.row);
         };
-        return <Button onClick={onClick}>Click</Button>;
+        return <Button onClick={onClick} sx={{backgroundColor:"yellow", color:"black",  borderRadius: 8,}}>view</Button>;
       },
     },
   ];
@@ -80,7 +162,7 @@ const VendorProjectTable: FC = () => {
   };
 
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div style={{ height: 600, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
