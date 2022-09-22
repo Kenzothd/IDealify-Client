f0c4755d917ba2c98132ccb5f1cc4e37ff16fd35
Clovis Fong <fongclovis@hotmail.com>
2022-09-22 14:36:27 +0800

    added client login page

--- src/pages/ClientLogIn.tsx
+++ src/pages/ClientLogIn.tsx
@@ -1,7 +1,47 @@
+import { Container, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
+import ClientLoginForm from "../components/client/ClientLoginForm";
 
 const ClientLogIn: FC = () => {
-  return <h1>Client Login</h1>;
+  return (
+    <Box>
+      <Grid container>
+        <Grid item xs={6} sx={{
+          background: 'grey',
+          height: '100vh'
+
+        }}>
+
+        </Grid>
+        <Grid item xs={6} sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          p: '2rem',
+
+        }}
+
+        >
+          <Box sx={{ mb: '3rem' }}>
+            <Typography variant='h2'>Hello</Typography>
+            <Typography variant='h2'>Homeowners,</Typography>
+          </Box>
+          <ClientLoginForm />
+          <Box>
+            <Typography variant='body2' sx={{ mt: '1rem' }}>Not a member yet?
+              <Link to='/vendor/sign-up' >
+                SIGN UP</Link></Typography>
+          </Box>
+
+        </Grid>
+      </Grid>
+
+
+    </Box>
+  )
 };
 
 export default ClientLogIn;
