22a7ca1668287ddc9a5adc620055f972913c6015
faithyeenx5 <faith.ye@hotmail.com>
2023-01-17 15:37:06 +0800

    updated test account for client

--- src/pages/ClientLogIn.tsx
+++ src/pages/ClientLogIn.tsx
@@ -77,7 +77,7 @@ const ClientLogIn: FC = () => {
                 onClick={() => {
                   console.log("clicked");
                   setUseTestUser({
-                    username: "testHomeownerAccount",
+                    username: "mockHomeownerAccount",
                     password: "Password123!",
                   });
                 }}
